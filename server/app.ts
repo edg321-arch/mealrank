f6df68f656273db7dfd797b86b73a78b09b2fc93
Elijah Goldberg <elijahgoldberg@MacBook-Air-150.local>
2026-01-29 13:45:51 -0700

    Fix tsconfig and imports

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express from "express";
+import express, { type NextFunction, type Request, type Response } from "express";
 import cors from "cors";
 import { api } from "./routes.js";
 
@@ -9,11 +9,11 @@ app.use(express.json({ limit: "10mb" }));
 
 app.use("/api", api);
 
-app.use((_req, res) => {
+app.use((_req: Request, res: Response) => {
   res.status(404).json({ error: "Not found" });
 });
 
-app.use((err: unknown, _req: express.Request, res: express.Response, _next: express.NextFunction) => {
+app.use((err: unknown, _req: Request, res: Response, _next: NextFunction) => {
   console.error(err);
   res.status(500).json({ error: "Internal server error" });
 });
